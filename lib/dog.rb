fde0d4ab5af163cf6c380f6bd187f5f2a38e8779
Charlie Oster <oster.charlie@yahoo.com>
2019-10-12 20:44:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,23 +1,9 @@
-class Dog 
-  def initialize(name)
-    @name = name
-  end
-  
-  def breed=(breed)
-    @breed = breed
-  end
-  
-  def breed
-     @breed
-    end
-  end
-  
-  fido = Dog.new
-  fido.name = "Fido"
-  fido.name
-  
-  fido.breed = "Pug"
-  fido.breed 
+class Dog
+def initialize(name, breed = "Mutt")
+  @name = name
+  @breed = breed
+end
+end
 
   
   
